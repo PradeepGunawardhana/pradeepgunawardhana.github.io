74261613e26d5d313118889b6af3e09c5ca146fe
Rangana Pradeep <ranganapradeepgunawardhana@gmail.com>
2025-02-07 21:57:24 +0530

    Updated source code new 2

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -14,7 +14,7 @@ function Contact() {
                     <div className="col-lg-12 col-12 details-wrapper " data-aos="fade-up"
                         data-aos-anchor-placement="center-bottom" data-aos-duration="1500">
 
-                        <div className="details-container d-flex mb-5 justify-content-center ">
+                        <div className="details-container d-flex mb-5 justify-content-start ">
                             <div className="icon-container">
                                 <i className="bi bi-geo-alt"></i>
                             </div>
